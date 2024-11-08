78cdd431a8e7e5d23d7ef2ea91af06b40d6a7e9a
Imene KOLLI <53266529+imene-swaan@users.noreply.github.com>
2024-11-08 10:42:20 +0100

    ignore

--- parseval/core.py
+++ parseval/core.py
@@ -2,153 +2,77 @@ from parseval.crawl import WebsiteCrawler
 import html2markdown
 import html2text
 import markdownify
-import os
 from parseval.parse import PDFParser
 
 
 CHROME_PATH = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
-output_dir = 'adobe_as_img'
+output_dir = 'data/finance'
 
-scraper = WebsiteCrawler(CHROME_PATH)
-
-
+# scraper = WebsiteCrawler(CHROME_PATH)
 
 
-scraper.run(
-        url = 'https://www.adobe.com/about-adobe.html',
-        output_dir = output_dir,
-        convert_to_pdf = True,
-        convert_to_html = True,
-        use_screenshot_method = True,
-        output_pdf_name = 'adobe.pdf',
+# # Scanned PDF
+# scraper.run(
+#         url = 'https://www.adobe.com/about-adobe.html',
+#         output_dir = output_dir,
+#         convert_to_pdf = True,
+#         convert_to_html = True,
+#         use_screenshot_method = False,
+#         output_pdf_name = 'adobe_crawled.pdf',
 
-    )
+#     )
 
-pdf_file_path = scraper.pdf_output_path
-html_filename = scraper.html_filename
-# Parse the PDF file
-parser = PDFParser()
-text_md, tables, images = parser.run(pdf_file_path)
+pdf_file_path = "data/finance/2006-04-17_10-K_d10k.pdf" 
+# "data/adobe/adobe_crawled.pdf"
+# "data/adobe/adobe.pdf"
+# scraper.pdf_output_path
+# "data/finance/2006-04-17_10-K_d10k.pdf" 
+# "data/arxiv/2404.12720v1.pdf" 
+# html_filename = scraper.html_filename
+# html_content = scraper.get_html_content()
+# html_metadata = scraper.meta_data
 
-# save the text content to a file
-with open(f"{output_dir}/{html_filename.split('.')[0]}_parsed.md", "w") as f:
-    f.write(text_md)
-
-
-html_file_path = os.path.join(output_dir, html_filename)
-html_content = scraper.get_html_content()
-html_metadata = scraper.meta_data
-
-
-
-markdown_content = html2markdown.convert(html_content)
-with open(f"{output_dir}/{html_filename.split('.')[0]}_html2md.md", "w") as f:
-    f.write(markdown_content)
+print(f"PDF file path: {pdf_file_path}")
+# Parse the PDF file to markdown with docling
+# parser = PDFParser()
+# output = parser.run(pdf_file_path, embed_images=False)[0]
 
 
-markdown_content = markdownify.markdownify(html_content)
-with open(f"{output_dir}/{html_filename.split('.')[0]}_markdownify.md", "w") as f:
-    f.write(markdown_content)
+html_filename = pdf_file_path.split('/')[-1].replace('.pdf', '.html')
+# # save the text content to a file
+# with open(f"{output_dir}/{html_filename.split('.')[0]}_docling.md", "w") as f:
+#     f.write(output.text)
 
 
-markdown_content = html2text.html2text(html_content)
-with open(f"{output_dir}/{html_filename.split('.')[0]}_html2txt.md", "w") as f:
-    f.write(markdown_content)
-
-
-
-
-
-
-output_dir = 'adobe_as_css'
-
-scraper = WebsiteCrawler(CHROME_PATH)
-
-scraper.run(
-        url = 'https://www.adobe.com/about-adobe.html',
-        output_dir = output_dir,
-        convert_to_pdf = True,
-        convert_to_html = True,
-        use_screenshot_method = False,
-        output_pdf_name = 'adobe.pdf',
-
-    )
-
-pdf_file_path = scraper.pdf_output_path
-html_filename = scraper.html_filename
-
-parser = PDFParser()
-text_md, tables, images = parser.run(pdf_file_path)
+# Parse the PDF file to markdown with llama parse
+parser = PDFParser(parser='pymupdf')
+output = parser.run(pdf_file_path)[0]
 
 # save the text content to a file
-with open(f"{output_dir}/{html_filename.split('.')[0]}_parsed.md", "w") as f:
-    f.write(text_md)
+with open(f"{output_dir}/{html_filename.split('.')[0]}_pymupdf.md", "w") as f:
+    f.write(output.text)
+
+# exit()
+# read the html content
+# with open(f"{output_dir}/{html_filename}", "r") as f:
+#     html_content = f.read()
 
 
-html_file_path = os.path.join(output_dir, html_filename)
-html_content = scraper.get_html_content()
-html_metadata = scraper.meta_data
+# # Convert the HTML content to markdown
+# markdown_content = html2markdown.convert(html_content)
+# with open(f"{output_dir}/{html_filename.split('.')[0]}_html2md.md", "w") as f:
+#     f.write(markdown_content)
 
 
-
-markdown_content = html2markdown.convert(html_content)
-with open(f"{output_dir}/{html_filename.split('.')[0]}_html2md.md", "w") as f:
-    f.write(markdown_content)
-
-
-markdown_content = markdownify.markdownify(html_content)
-with open(f"{output_dir}/{html_filename.split('.')[0]}_markdownify.md", "w") as f:
-    f.write(markdown_content)
+# markdown_content = markdownify.markdownify(html_content, bullets="-")
+# with open(f"{output_dir}/{html_filename.split('.')[0]}_markdownify.md", "w") as f:
+#     f.write(markdown_content)
 
 
-markdown_content = html2text.html2text(html_content)
-with open(f"{output_dir}/{html_filename.split('.')[0]}_html2txt.md", "w") as f:
-    f.write(markdown_content)
-
-
-
-
-output_dir = 'adobe_no_embed'
-
-scraper = WebsiteCrawler(CHROME_PATH)
-
-scraper.run(
-        url = 'https://www.adobe.com/about-adobe.html',
-        output_dir = output_dir,
-        convert_to_pdf = True,
-        convert_to_html = True,
-        use_screenshot_method = False,
-        output_pdf_name = 'adobe.pdf',
-
-    )
-
-pdf_file_path = scraper.pdf_output_path
-html_filename = scraper.html_filename
-
-parser = PDFParser()
-text_md, tables, images = parser.run(pdf_file_path, embed_images=False)
-
-# save the text content to a file
-with open(f"{output_dir}/{html_filename.split('.')[0]}_parsed.md", "w") as f:
-    f.write(text_md)
-
-
-html_file_path = os.path.join(output_dir, html_filename)
-html_content = scraper.get_html_content()
-html_metadata = scraper.meta_data
+# markdown_content = html2text.html2text(html_content)
+# with open(f"{output_dir}/{html_filename.split('.')[0]}_html2txt.md", "w") as f:
+#     f.write(markdown_content)
 
 
 
-markdown_content = html2markdown.convert(html_content)
-with open(f"{output_dir}/{html_filename.split('.')[0]}_html2md.md", "w") as f:
-    f.write(markdown_content)
 
-
-markdown_content = markdownify.markdownify(html_content)
-with open(f"{output_dir}/{html_filename.split('.')[0]}_markdownify.md", "w") as f:
-    f.write(markdown_content)
-
-
-markdown_content = html2text.html2text(html_content)
-with open(f"{output_dir}/{html_filename.split('.')[0]}_html2txt.md", "w") as f:
-    f.write(markdown_content)
