715d51dc880579aeddeb10dbbf19b5f3d794321f
Imene KOLLI <53266529+imene-swaan@users.noreply.github.com>
2024-11-12 13:00:26 +0100

    parse html to md

--- parseval/parse.py
+++ parseval/parse.py
@@ -4,8 +4,17 @@ from .parsers.pymupdf_parser import PyMuPDFParser
 from typing import Literal, List, Union
 from .parsers.schema import ParserOutput
 
+import html2markdown
+import html2text
+import markdownify
+
+
+
 class PDFParser:
-    def __init__(self, parser: Literal["docling", "llama", "pymupdf"] = "docling"):
+    def __init__(
+            self, 
+            parser: Literal["docling", "llama", "pymupdf"] = "docling"
+            ):
         
         if parser == "docling":
             self.parser = DoclingPDFParser()
@@ -17,7 +26,11 @@ class PDFParser:
             raise ValueError("Invalid parser specified. Please use 'docling' or 'llama'.")
         
         
-    def run(self, pdf_path: Union[str, List[str]], **kwargs) -> List[ParserOutput]:
+    def run(
+            self, 
+            pdf_path: Union[str, List[str]], 
+            **kwargs
+            ) -> List[ParserOutput]:
         """
         Run the PDF parser on the given PDF file.
         """
@@ -25,4 +38,31 @@ class PDFParser:
         outputs = self.parser.parse_and_export(pdf_path, **kwargs)
 
         return outputs
+
+
+class HTMLParser:
+    def __init__(self):
+        pass
+
+    def run(
+            self, 
+            html_content: str, parser: Literal["markdownify", "html2markdown", "html2text"] = "markdownify",
+            kwargs: dict = {}
+            ) -> str:
+        """
+        Run the HTML parser on the given HTML content.
+        """
+        markdown_content = ""
+
+        if parser == "markdownify":
+            markdown_content = markdownify.markdownify(html_content, **kwargs)
+        elif parser == "html2markdown":
+            markdown_content = html2markdown.convert(html_content)
+        elif parser == "html2text":
+            markdown_content = html2text.html2text(html_content, **kwargs)
+        else:
+            raise ValueError("Invalid parser specified. Please use 'markdownify', 'html2markdown' or 'html2text'.")
+        
+        return markdown_content
+
 
