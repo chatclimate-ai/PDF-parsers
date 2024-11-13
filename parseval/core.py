6af59702f020fa9983468238bf6f787be3db5cb0
Imene KOLLI <53266529+imene-swaan@users.noreply.github.com>
2024-11-13 14:39:52 +0100

    full pipeline untested

--- parseval/core.py
+++ parseval/core.py
@@ -1,8 +1,8 @@
 from parseval.crawl import WebsiteCrawler
 from parseval.parse import PDFParser, HTMLParser
-from parseval.parsers.schema import ParserOutput
-from parseval.evaluate import EvaluateParser
-from typing import List, Dict, Tuple
+from parseval.schemas import GroundTruth, Predictions, Metadata
+from parseval.evaluate import ParserMetrics
+from typing import List, Dict
 import json
 
 
@@ -12,27 +12,30 @@ class PDFParsingPipeline:
     pdf_parser: PDFParser
     html_parser: HTMLParser
     crawler: WebsiteCrawler
+    evaluator: ParserMetrics
 
     def __init__(
             self, 
             pdf_parser: str = 'pymupdf', 
-            html_parser: str = 'html2text'
+            html_parser: str = 'html2text',
+            eval_method: str = 'lcs'
             ):
         
         self.pdf_parser = PDFParser(parser=pdf_parser)
         self.html_parser = HTMLParser(parser=html_parser)
         self.crawler = WebsiteCrawler(CHROME_PATH)
+        self.evaluator = ParserMetrics(method=eval_method)
 
     
     def get_html_contents(
             self, 
             input_files: List[Dict],
             output_dir: str
-            ) -> Tuple[List[str], List[str], List[Dict]]:
+            ) -> GroundTruth:
         
         html_paths: List[str] = []
         html_contents: List[str] = []
-        metadatas: List[Dict] = []
+        metadatas: List[Metadata] = []
 
         for file_data in input_files:
             file_path: str = file_data['file_path']
@@ -46,7 +49,7 @@ class PDFParsingPipeline:
                 )
                 html_filepath = self.crawler.get_html_filepath()
                 html_content = self.crawler.get_html_content()
-                metadata = self.crawler.get_meta_data()
+                metadata: Metadata = self.crawler.get_meta_data()
 
            
             else:
@@ -62,7 +65,7 @@ class PDFParsingPipeline:
                     metadata_path = html_filepath.split('/')[:-1] + ['metadata.json']
                     metadata_path = '/'.join(metadata_path)
                     with open(metadata_path, "r") as f:
-                        metadata = json.load(f)
+                        metadata: Metadata = json.load(f)
                 
                 except FileNotFoundError:
                     raise FileNotFoundError(f"Metadata file not found at {metadata_path}")
@@ -73,7 +76,7 @@ class PDFParsingPipeline:
             html_contents.append(html_content)
             metadatas.append(metadata)
 
-        return html_paths, html_contents, metadatas
+        return GroundTruth(html_paths=html_paths, html_contents=html_contents, metadatas=metadatas)
 
 
     def get_parsed_text(
@@ -105,8 +108,11 @@ class PDFParsingPipeline:
 
             pdf_paths.append(pdf_filepath)
         
-        parsing_outputs: List[ParserOutput] = self.pdf_parser.run(pdf_paths, modalities=modalities, **pdf_parsing_options)
-        return [output.text for output in parsing_outputs]
+        parsing_outputs: Predictions = self.pdf_parser.run(pdf_paths, modalities=modalities, **pdf_parsing_options)
+
+        # TODO: Remove table md from the text before evaluation, evaluate only the text and the tables separately
+
+        return parsing_outputs
 
 
     def get_ground_truth(
@@ -114,27 +120,55 @@ class PDFParsingPipeline:
             input_files: List[Dict],
             output_dir: str,
             html_parsing_options: Dict = {},
-            ) -> List[str]:
+            ) -> GroundTruth:
         
         # For now, we use only the text modality
-        _, html_contents, _ = self.get_html_contents(input_files, output_dir)
+        ground_truth= self.get_html_contents(input_files, output_dir)
 
-        ground_truths: List[str] = []
-        for html_content in html_contents:
+        for i, html_content in enumerate(ground_truth.html_contents):
             output = self.html_parser.run(html_content, **html_parsing_options)
-            ground_truths.append(output)
+            ground_truth.html_contents[i] = output
 
-
-        return ground_truths
+        return ground_truth
 
 
     
 
-    def evaluate_parser(
+    def evaluate(
             self,
-            ground_truths: List[str],
-            parsing_outputs: List[str],
-            evaluation_method: str = 'lcs',
+            ground_truths: GroundTruth,
+            parsing_outputs: Predictions,
+            output_dir: str,
+            evaluation_options: Dict = {}
             ) -> List[Dict]:
         
-        pass
+
+        # Evaluate the parser text output only for now
+        self.evaluator.evaluate_text(ground_truths, parsing_outputs, **evaluation_options)
+        self.evaluator.save_evaluation(output_dir)
+        self.evaluator.save_aggregation(output_dir)
+
+
+    
+    def run(
+            self,
+            input_files: List[Dict],
+            output_dir: str,
+            pdf_parsing_options: Dict = {},
+            html_parsing_options: Dict = {},
+            crawler_options: Dict = {},
+            evaluation_options: Dict = {}
+            ) -> None:
+        
+        ground_truth = self.get_ground_truth(input_files, output_dir, html_parsing_options)
+        parsing_outputs = self.get_parsed_text(input_files, output_dir, pdf_parsing_options, crawler_options)
+
+        self.evaluate(ground_truth, parsing_outputs, output_dir, evaluation_options)
+
+        return None
+
+
+
+
+    
+    
