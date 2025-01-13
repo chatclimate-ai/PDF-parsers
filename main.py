649b076c67409cb9882848feb6444537044eb9b1
Imene KOLLI <53266529+imene-swaan@users.noreply.github.com>
2025-01-13 15:27:32 +0100

    set configs from toml

--- main.py
+++ main.py
@@ -1,51 +1,29 @@
 import json
 from parseval.core import PDFParsingPipeline
+import toml
 
 
-input_files = "data/test/v4/input_files.json"
-with open(input_files, "r") as f:
+VERSION = "v5"
+
+
+with open(f"data/test/{VERSION}/input_files.json", "r") as f:
     input_files = json.load(f)
+output_dir = f"data/test/{VERSION}"
 
-output_dir = "data/test/v4"
-
+CONFIG = toml.load("config.toml")
 
 if __name__ == "__main__":
     pipeline = PDFParsingPipeline(
-        pdf_parser="docling",
-        html_parser="html2text",
-        eval_method="rougeL"
+        pdf_parser= CONFIG["tool"]["pdf_parser"]["name"],
+        html_parser= CONFIG["tool"]["html_parser"]["name"],
+        eval_method= CONFIG["tool"]["evaluation"]["name"]
 
     )
     pipeline.run(
         input_files = input_files,
         output_dir = output_dir,
-        pdf_parsing_options = {
-            # "do_ocr":True,
-            # "ocr_options":"easyocr",
-            # "do_table_structure":True,
-            # "do_cell_matching":False,
-            # "tableformer_mode":"ACCURATE",
-            # "images_scale":1.0,
-            # "generate_page_images":False,
-            # "generate_picture_images":False,
-            # "generate_table_images":False,
-            # "backend":"docling",
-            # "embed_images":False
-        },
-        html_parsing_options = {
-            "heading_style":"ATX"
-        },
-        # crawler_options = {
-        #     "use_screenshot_method":True
-        # },
-        evaluation_options = {
-            "method": "chunk_grid",
-            "step_size":10,
-            "window_size":100,
-            "threshold":0.8,
-            "preprocess":True,
-        }
+        pdf_parsing_options = CONFIG["pdf_parser"][CONFIG["tool"]["pdf_parser"]["name"]],
+        html_parsing_options = CONFIG["html_parser"][CONFIG["tool"]["html_parser"]["name"]],
+        crawler_options = CONFIG["crawler"]["options"] if CONFIG["tool"]["crawler"]["use"] else {},
+        evaluation_options = CONFIG["evaluation"]["options"]
         )
-
-
-# method: Literal["chunk_gt", "chunk_md", "chunk_both", "chunk_grid"] = "chunk_gt"
