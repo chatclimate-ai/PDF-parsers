e09758de095cfeaffed9c0ba93933288bedd6bed
Imene KOLLI <53266529+imene-swaan@users.noreply.github.com>
2025-01-13 14:34:27 +0100

    rouge L evaluation metric

--- parseval/evaluation/methods/rouge.py
+++ parseval/evaluation/methods/rouge.py
@@ -1,8 +1,12 @@
-
-
-
+from rouge import Rouge
 
 
 class RougeLEvaluator:
-    def __init__(self, ngram=2):
-        self.ngram = ngram
+    def __init__(self):
+        self.rouge = Rouge()
+
+    def similarity_score(self, text1: str, text2: str) -> float:
+        # Calculate the ROUGE-L score between two texts
+        scores = self.rouge.get_scores(text1, text2)
+        return scores[0]['rouge-l']['f']
+
